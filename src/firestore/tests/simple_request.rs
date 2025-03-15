cdde19f8503c936da95c437c92fa74b00fabb969
Carlos O'Ryan <coryan@google.com>
2025-03-15 11:46:35 -0400

    WIP - first successful request

---
+++ src/firestore/tests/simple_request.rs
@@ -0,0 +1,58 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use google_cloud_firestore::client::Firestore;
+use google_cloud_firestore::model;
+use google_cloud_firestore::Error;
+use google_cloud_firestore::Result;
+
+/// Returns the project id used for the integration tests.
+pub fn project_id() -> Result<String> {
+    let project_id = std::env::var("GOOGLE_CLOUD_PROJECT").map_err(Error::other)?;
+    Ok(project_id)
+}
+
+pub async fn hello_world() -> Result<()> {
+    let project_id = "coryan-test".to_string(); // project_id()?;
+    let client = Firestore::new().await?;
+    let response = client
+        .create_document(
+            format!("projects/{project_id}/databases/(default)/documents"),
+            "greeting",
+        )
+        .set_document(model::Document::new().set_fields([(
+            "greeting",
+            model::Value::new().set_string_value("Hello World!"),
+        )]))
+        .send()
+        .await?;
+    println!("SUCCESS on create_document: {response:?}");
+    Ok(())
+}
+
+#[cfg(test)] // #[cfg(all(test, feature = "run-integration-tests"))]
+mod driver {
+    use super::Error;
+    use super::Result;
+
+    fn report(e: Error) -> Error {
+        println!("\nERROR {e}\n");
+        Error::other("test failed")
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn run_hello_world() -> Result<()> {
+        super::hello_world().await.map_err(report)
+    }
+}
