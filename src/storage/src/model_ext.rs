cd38bbf4ea90e4ba0b7430ae8698286cc15f4960
Carlos O'Ryan <coryan@google.com>
2025-10-28 14:16:41 -0400

    WIP - start thinking about ReadRange translation

--- src/storage/src/model_ext.rs
+++ src/storage/src/model_ext.rs
@@ -260,6 +260,27 @@ impl ReadRange {
             limit: count,
         })
     }
+
+    #[cfg(google_cloud_unstable_storage_bidi)]
+    pub(crate) fn to_bidi_range(self) -> crate::google::storage::v2::ReadRange {
+        use crate::google::storage::v2::ReadRange as Proto;
+        match self.0 {
+            Range::All => Proto::default(),
+            Range::Offset(o) => Proto {
+                read_offset: o as i64,
+                ..Proto::default()
+            },
+            Range::Tail(c) => Proto {
+                read_offset: -(c as i64),
+                ..Proto::default()
+            },
+            Range::Segment { offset, limit } => Proto {
+                read_offset: offset as i64,
+                read_length: limit as i64,
+                ..Proto::default()
+            },
+        }
+    }
 }
 
 impl crate::model::ReadObjectRequest {
--- src/storage/src/storage/bidi/object_descriptor.rs
+++ src/storage/src/storage/bidi/object_descriptor.rs
@@ -12,20 +12,126 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use crate::google::storage::v2::BidiReadObjectResponse;
 use crate::model::Object;
+use crate::{Error, Result};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc::{Receiver, Sender};
 
 /// Represents an open object in Cloud Storage.
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct ObjectDescriptor {
     object: Object,
+    inner: Arc<Mutex<ObjectDescriptorInner>>,
 }
 
 impl ObjectDescriptor {
     pub(crate) fn new(object: Object) -> Self {
-        Self { object }
+        Self {
+            object,
+            inner: Arc::new(Mutex::new(ObjectDescriptorInner::new())),
+        }
     }
 
     pub fn object(&self) -> &Object {
         &self.object
     }
+
+    pub async fn read_range(&mut self, range: crate::model_ext::ReadRange) -> RangeResponse {
+        self.inner
+            .clone()
+            .lock()
+            .expect("never poisoned")
+            .read_range(range)
+            .await
+    }
+
+    async fn background(
+        &self,
+        mut source: tonic::codec::Streaming<BidiReadObjectResponse>,
+    ) -> Result<()> {
+        let inner = self.inner.clone();
+        while let Some(m) = source.message().await.transpose() {
+            match m {
+                Err(e) => {
+                    for (_, pending) in inner.lock().expect("never poisoned").ranges.drain() {
+                        let _ = pending
+                            .tx
+                            .send(Err(Error::io(format!("TODO - pass on the error: {e:?}"))))
+                            .await;
+                    }
+                }
+                Ok(r) => {
+                    for d in r.object_data_ranges {
+                        let (Some(range), Some(data)) = (d.read_range, d.checksummed_data) else {
+                            continue;
+                        };
+                        let id = range.read_id;
+                        let mut guard = inner.lock().expect("never poisoned");
+
+                        let payload = Payload {
+                            data: data.content,
+                            offset: range.read_offset,
+                        };
+                        if d.range_end {
+                            if let Some(p) = guard.ranges.remove(&id) {
+                                let _ = p.tx.send(Ok(payload)).await;
+                            }
+                        } else {
+                            if let Some(p) = guard.ranges.get(&id) {
+                                let _ = p.tx.send(Ok(payload)).await;
+                            }
+                        };
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct ObjectDescriptorInner {
+    read_id: i64,
+    ranges: HashMap<i64, PendingRangeRead>,
+}
+
+impl ObjectDescriptorInner {
+    pub fn new() -> Self {
+        Self {
+            read_id: 0,
+            ranges: HashMap::new(),
+        }
+    }
+
+    pub async fn read_range(&mut self, range: crate::model_ext::ReadRange) -> RangeResponse {
+        let (tx, rx) = tokio::sync::mpsc::channel(1);
+        let id = self.read_id;
+        let pending = PendingRangeRead { range, tx };
+        self.read_id += 1;
+        self.ranges.insert(id, pending);
+        RangeResponse { rx }
+    }
+}
+
+pub struct RangeResponse {
+    rx: Receiver<Result<Payload>>,
+}
+
+impl RangeResponse {
+    pub async fn next(&mut self) -> Option<Result<Payload>> {
+        self.rx.recv().await
+    }
+}
+
+pub struct Payload {
+    pub data: bytes::Bytes,
+    pub offset: i64,
+}
+
+#[derive(Debug)]
+struct PendingRangeRead {
+    range: crate::model_ext::ReadRange,
+    tx: Sender<Result<Payload>>,
 }
